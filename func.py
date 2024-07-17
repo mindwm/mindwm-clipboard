9759f345fb88a5953d8adfe0d81c8c23a3124f0a
Your Name <you@example.com>
2024-07-17 18:19:07 +0000

    Load clipboard payload using auto-generated python code by openapi-generator

--- func.py
+++ func.py
@@ -1,34 +1,29 @@
 import sys
 import os
-from cloudevents.http import from_http
 import sys
 import os
 import re
 sys.path.append(os.path.abspath('mindwm-api/neomodel'))
 sys.path.append(os.path.abspath('mindwm-api/generated/openapi/python/'))
-#sys.path.append(os.path.join(os.path.dirname(__file__), './mindwm-api/generated/openapi/python/MindwWM/models/'))
 
 from parliament import Context, event
 
 import neomodel_data
-import MindwWM
+import MindWM
+import pprint
+
+from cloudevents.http import from_http
+from cloudevents import abstract, conversion
 
 
 
 @event
 def main(context: Context):
 
-    """
-    Function template
-    The context parameter contains the Flask request object and any
-    CloudEvent received with the request.
-    """
     event = from_http(context.request.headers, context.request.data)
-    print(f'XXX', file=sys.stderr)
-    print(f'{event.data}', file=sys.stderr)
+    pprint.pprint(conversion.to_json(event), stream=sys.stderr)
+    clipboard = MindWM.Clipboard.from_json(conversion.to_json(event))
+    pprint.pprint(event, stream=sys.stderr)
+    print(clipboard.to_str(), file=sys.stderr)
 
-    # Add your business logic here
-
-    # The return value here will be applied as the data attribute
-    # of a CloudEvent returned to the function invoker
     return context.cloud_event.data
